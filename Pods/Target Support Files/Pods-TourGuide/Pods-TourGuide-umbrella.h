9c802c73763964c194ab9c17fd4cdc56f180eb1f
Lauren N. Roth <laurenroth@gmail.com>
2016-10-08 19:03:25 -0400

    Installs SwiftyJSON, Alamofire, and GoogleMaps pods.

---
+++ Pods/Target Support Files/Pods-TourGuide/Pods-TourGuide-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_TourGuideVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_TourGuideVersionString[];
+
---
+++ Pods/Target Support Files/Pods-TourGuideUITests/Pods-TourGuideUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TourGuideUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TourGuideUITests
+@end
